ebaa763e2b0f79cba8aed251037dc994e6de1abd
jpmiii <git@jpmiii.com>
2013-11-13 10:35:25 -0600

    added recipes

--- src/com/jpmiii/RealmsRecipes/RealmsRecipes.java
+++ src/com/jpmiii/RealmsRecipes/RealmsRecipes.java
@@ -1,19 +1,29 @@
 package com.jpmiii.RealmsRecipes;
 
 import java.util.Iterator;
+import java.util.Map;
 
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.ShapelessRecipe;
-import org.bukkit.material.MaterialData;
+import org.bukkit.inventory.meta.ItemMeta;
 
-public class RealmsRecipes extends JavaPlugin {
+public class RealmsRecipes extends JavaPlugin implements Listener {
 
 	public void onEnable() {
+
+		getServer().getPluginManager().registerEvents(this, this);
 
 		// CraftingManager.getInstance().recipes;
 		final Iterator<Recipe> recipes = getServer().recipeIterator();
@@ -26,64 +36,219 @@ public class RealmsRecipes extends JavaPlugin {
 
 			if ((result = recipe.getResult()) == null)
 				continue;
-			// remove diamond chestplate
-			if (result.getType() == Material.DIAMOND_CHESTPLATE)
-				recipes.remove();
-			// remove diamond helmet
-			if (result.getType() == Material.DIAMOND_HELMET)
-				recipes.remove();
-			// remove diamond leggings
-			if (result.getType() == Material.DIAMOND_LEGGINGS)
-				recipes.remove();
+			/*
+			 * / remove diamond chestplate if (result.getType() ==
+			 * Material.DIAMOND_CHESTPLATE) recipes.remove(); // remove diamond
+			 * helmet if (result.getType() == Material.DIAMOND_HELMET)
+			 * recipes.remove(); // remove diamond leggings if (result.getType()
+			 * == Material.DIAMOND_LEGGINGS) recipes.remove();
+			 */
 		}
 
-		// Craft diamond chestplate
-
-		ShapedRecipe recipe1 = new ShapedRecipe(new ItemStack(
-				Material.DIAMOND_CHESTPLATE, 1));
-		recipe1.shape("A A", "ABA", "AAA");
-		recipe1.setIngredient('A', Material.DIAMOND);
-		recipe1.setIngredient('B', Material.DIAMOND_BLOCK);
-		getServer().addRecipe(recipe1);
-
-		// uncraft diamond chestplate
-
-		ShapelessRecipe recipe2 = new ShapelessRecipe(new ItemStack(
-				Material.DIAMOND, 6));
-		recipe2.addIngredient(1, Material.DIAMOND_CHESTPLATE);
-		getServer().addRecipe(recipe2);
-
-		// craft diamond leggings
-
-		ShapedRecipe recipe3 = new ShapedRecipe(new ItemStack(
-				Material.DIAMOND_LEGGINGS, 1));
-		recipe3.shape("ABA", "A A", "A A");
-		recipe3.setIngredient('A', Material.DIAMOND);
-		recipe3.setIngredient('B', Material.DIAMOND_BLOCK);
-		getServer().addRecipe(recipe3);
-
-		// uncraft diamond leggings
-
-		ShapelessRecipe recipe6 = new ShapelessRecipe(new ItemStack(
-				Material.DIAMOND, 5));
-		recipe6.addIngredient(1, Material.DIAMOND_LEGGINGS);
-		getServer().addRecipe(recipe6);
-
-		ShapedRecipe recipe5 = new ShapedRecipe(new ItemStack(
-				Material.DIAMOND_HELMET, 1));
-		recipe5.shape("ABA", "A A", "   ");
-		recipe5.setIngredient('A', Material.DIAMOND);
-		recipe5.setIngredient('B', Material.DIAMOND_BLOCK);
-
-		getServer().addRecipe(recipe5);
-
-		ShapelessRecipe recipe8 = new ShapelessRecipe(new ItemStack(
-				Material.DIAMOND, 3));
-		recipe8.addIngredient(1, Material.DIAMOND_HELMET);
-
-		getServer().addRecipe(recipe8);
-
 		getLogger().info("onEnable has been invoked!");
+
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void invarmorEvent(InventoryClickEvent event) {
+		ItemStack enar = event.getCurrentItem();
+		String aname = null;
+		if (enar.hasItemMeta()) {
+			aname = enar.getItemMeta().getDisplayName();
+		}
+
+		getLogger().info("1 " + event.getInventory().getType().toString());
+		getLogger().info("1a " + event.getInventory().getSize());
+
+		if (event.getInventory().getType() == InventoryType.WORKBENCH) {
+
+			ItemStack[] is = event.getInventory().getContents();
+
+			if (is[1] != null) {
+				//
+				// all armor recipes have 16 emerald blocks in slot 1
+				//
+				if (is[1].equals(new ItemStack(Material.EMERALD_BLOCK, 16))) {
+					//
+					// Helm recipe
+					//
+					if (is[2].equals(new ItemStack(Material.NETHER_STAR, 1))
+							&& is[3].equals(new ItemStack(Material.EMERALD_BLOCK, 16))
+							&& is[4].equals(new ItemStack(Material.DIAMOND_BLOCK, 3))
+							&& is[6].equals(new ItemStack(Material.DIAMOND_BLOCK, 3))) {
+						ItemStack nit = new ItemStack(Material.DIAMOND_HELMET,	1);
+
+						ItemMeta im = nit.getItemMeta();
+
+						im.setDisplayName("Civrealms Helm");
+						nit.setItemMeta(im);
+
+						nit.addEnchantment(
+								Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+						nit.addEnchantment(Enchantment.OXYGEN, 3);
+						nit.addEnchantment(Enchantment.WATER_WORKER, 1);
+
+						event.getInventory().clear();
+						CraftingInventory ci = (CraftingInventory) event
+								.getInventory();
+						ci.setResult(nit);
+					}
+					//
+					// Chest plate recipe
+					//
+					if (       //is[2].equals(new ItemStack(Material.NETHER_STAR, 1))
+							//&& 
+							   is[3].equals(new ItemStack(Material.EMERALD_BLOCK, 16))
+							&& is[4].equals(new ItemStack(Material.DIAMOND_BLOCK, 2))
+							&& is[5].equals(new ItemStack(Material.RECORD_6, 1))
+							&& is[6].equals(new ItemStack(Material.DIAMOND_BLOCK, 2))
+							&& is[7].equals(new ItemStack(Material.DIAMOND_BLOCK, 1))
+							&& is[8].equals(new ItemStack(Material.DIAMOND_BLOCK, 1))
+							&& is[9].equals(new ItemStack(Material.DIAMOND_BLOCK, 1))
+							)
+					{
+						ItemStack nit = new ItemStack(Material.DIAMOND_CHESTPLATE,	1);
+
+						ItemMeta im = nit.getItemMeta();
+
+						im.setDisplayName("Civrealms Plate");
+						nit.setItemMeta(im);
+
+						nit.addEnchantment(
+								Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+						//nit.addEnchantment(Enchantment.OXYGEN, 3);
+						//nit.addEnchantment(Enchantment.WATER_WORKER, 1);
+
+						event.getInventory().clear();
+						CraftingInventory ci = (CraftingInventory) event
+								.getInventory();
+						ci.setResult(nit);
+					}
+					//
+					// pants recipe
+					//
+					if (       is[2].equals(new ItemStack(Material.RECORD_11, 1))
+							&& is[3].equals(new ItemStack(Material.EMERALD_BLOCK, 16))
+							&& is[4].equals(new ItemStack(Material.DIAMOND_BLOCK, 2))
+							//&& is[5].equals(new ItemStack(Material.RECORD_6, 1))
+							&& is[6].equals(new ItemStack(Material.DIAMOND_BLOCK, 2))
+							&& is[7].equals(new ItemStack(Material.DIAMOND_BLOCK, 2))
+							//&& is[8].equals(new ItemStack(Material.DIAMOND_BLOCK, 1))
+							&& is[9].equals(new ItemStack(Material.DIAMOND_BLOCK, 2))
+							)
+					{
+						ItemStack nit = new ItemStack(Material.DIAMOND_LEGGINGS,	1);
+
+						ItemMeta im = nit.getItemMeta();
+
+						im.setDisplayName("Civrealms Pants");
+						nit.setItemMeta(im);
+
+						nit.addEnchantment(
+								Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+						//nit.addEnchantment(Enchantment.OXYGEN, 3);
+						//nit.addEnchantment(Enchantment.WATER_WORKER, 1);
+
+						event.getInventory().clear();
+						CraftingInventory ci = (CraftingInventory) event
+								.getInventory();
+						ci.setResult(nit);
+					}
+					//
+					// boots recipe
+					//
+					if (       is[2].equals(new ItemStack(Material.RECORD_3, 1))
+							&& is[3].equals(new ItemStack(Material.EMERALD_BLOCK, 16))
+							&& is[4].equals(new ItemStack(Material.DIAMOND_BLOCK, 2))
+							&& is[5].equals(new ItemStack(Material.RECORD_3, 1))
+							&& is[6].equals(new ItemStack(Material.DIAMOND_BLOCK, 2))
+							&& is[7].equals(new ItemStack(Material.GOLDEN_APPLE, 16))
+							&& is[8].equals(new ItemStack(Material.RECORD_3, 1))
+							&& is[9].equals(new ItemStack(Material.GOLDEN_APPLE, 16))
+							)
+					{
+						ItemStack nit = new ItemStack(Material.DIAMOND_BOOTS,	1);
+
+						ItemMeta im = nit.getItemMeta();
+
+						im.setDisplayName("Civrealms Boots");
+						nit.setItemMeta(im);
+
+						nit.addEnchantment(
+								Enchantment.PROTECTION_ENVIRONMENTAL, 4);
+						nit.addEnchantment(Enchantment.PROTECTION_FALL, 4);
+						//nit.addEnchantment(Enchantment.WATER_WORKER, 1);
+
+						event.getInventory().clear();
+						CraftingInventory ci = (CraftingInventory) event
+								.getInventory();
+						ci.setResult(nit);
+					}
+				}
+			}
+		}
+
+		if (event.getInventory().getType() == InventoryType.ANVIL) {
+
+			if (aname != null) {
+				if (aname.startsWith("Civrealms")) {
+					String bname = event.getInventory().getItem(0)
+							.getItemMeta().getDisplayName();
+					if (bname == null) {
+						event.setCancelled(true);
+					} else if (!bname.startsWith("Civrealms")) {
+						event.setCancelled(true);
+					}
+				}
+			}
+		}
+
+		if (enar.containsEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL)) {
+			getLogger().info("2");
+			if (enar.getType() == Material.DIAMOND_CHESTPLATE
+					|| enar.getType() == Material.DIAMOND_LEGGINGS
+					|| enar.getType() == Material.DIAMOND_HELMET
+					|| enar.getType() == Material.DIAMOND_BOOTS) {
+				getLogger().info("3");
+				if (enar.getEnchantmentLevel(Enchantment.PROTECTION_ENVIRONMENTAL) == 4) {
+					getLogger().info("4");
+
+					// String aname = enar.getItemMeta().getDisplayName();
+					if (aname == null) {
+						getLogger().info("5");
+						event.getCurrentItem().removeEnchantment(
+								Enchantment.PROTECTION_ENVIRONMENTAL);
+						event.getCurrentItem().addEnchantment(
+								Enchantment.PROTECTION_ENVIRONMENTAL, 3);
+					} else if (aname.startsWith("Civrealms") != true) {
+						getLogger().info("5");
+						event.getCurrentItem().removeEnchantment(
+								Enchantment.PROTECTION_ENVIRONMENTAL);
+						event.getCurrentItem().addEnchantment(
+								Enchantment.PROTECTION_ENVIRONMENTAL, 3);
+					}
+				}
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void enchantEvent(EnchantItemEvent event) {
+		getLogger().info("e1 " + event.getInventory().getType().toString());
+		Material am = event.getItem().getType();
+		if (am == Material.DIAMOND_CHESTPLATE
+				|| am == Material.DIAMOND_LEGGINGS
+				|| am == Material.DIAMOND_HELMET
+				|| am == Material.DIAMOND_BOOTS) {
+
+			Map<Enchantment, Integer> enchmap = event.getEnchantsToAdd();
+			if (enchmap.containsKey(Enchantment.PROTECTION_ENVIRONMENTAL)) {
+				if (enchmap.get(Enchantment.PROTECTION_ENVIRONMENTAL) == 4) {
+					event.getEnchantsToAdd().put(
+							Enchantment.PROTECTION_ENVIRONMENTAL, 3);
+				}
+			}
+		}
 
 	}
 
